d2f2359bb56c03db5a80c4c20f86cffa6159a26d
Breno Azevedo <brenorazevedo@hotmail.com>
2025-04-16 16:47:24 -0300

    text,llama,summarization

--- testando_text_to_image.py
+++ testando_text_to_image.py
@@ -1,6 +1,9 @@
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionPipeline
+import torch
 
-pipe = DiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5", safety_checker=None)
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float32)
+pipe = pipe.to("cpu")  # ou .to("cuda") se tiver GPU
 
-prompt = "a fantasy landscape with mountains and a river, in the style of Studio Ghibli"
+prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
 image = pipe(prompt).images[0]
+image.save("astronaut_rides_horse.png")
